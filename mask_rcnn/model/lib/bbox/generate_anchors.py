1706ca30dc92528f6b1f72347fb074392c0a591e
MengTianjian <tim40@pitt.edu>
2018-03-22 17:00:57 -0400

    debug

--- mask_rcnn/model/lib/bbox/generate_anchors.py
+++ mask_rcnn/model/lib/bbox/generate_anchors.py
@@ -24,6 +24,31 @@ def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
                          for i in xrange(ratio_anchors.shape[0])])
     return anchors
 
+def _whctrs(anchor):
+    """
+    Return width, height, x center, and y center for an anchor (window).
+    """
+
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+
+def _mkanchors(ws, hs, x_ctr, y_ctr):
+    """
+    Given a vector of widths (ws) and heights (hs) around a center
+    (x_ctr, y_ctr), output a set of anchors (windows).
+    """
+
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
+                         y_ctr - 0.5 * (hs - 1),
+                         x_ctr + 0.5 * (ws - 1),
+                         y_ctr + 0.5 * (hs - 1)))
+    return anchors
+
 def _ratio_enum(anchor, ratios):
     """
     Enumerate a set of anchors for each aspect ratio wrt an anchor.
